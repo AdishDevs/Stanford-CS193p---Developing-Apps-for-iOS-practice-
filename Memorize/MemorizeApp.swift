5a51bace70003a3cf0877676f13ef59f91d4c9d0
AdishDevs <adishkanimsara.adikari@gmail.com>
2024-12-30 19:25:16 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Admin on 2024-12-30.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by Admin on 2024-12-30.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
